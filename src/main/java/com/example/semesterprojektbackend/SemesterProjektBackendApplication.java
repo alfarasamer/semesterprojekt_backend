ce9a475840d7a9f84614fb3f947976896196417f
Fantasmarik <r.bereuter@live.at>
2021-06-17 11:19:13 +0200

    init

---
+++ src/main/java/com/example/semesterprojektbackend/SemesterProjektBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.semesterprojektbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemesterProjektBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemesterProjektBackendApplication.class, args);
+    }
+
+}
