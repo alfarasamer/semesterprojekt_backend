719fc437f10ae8f570a9c63903d20d403b71d3f1
alfarasamer <85562845+alfarasamer@users.noreply.github.com>
2021-06-17 13:07:14 +0200

    Commit1

---
+++ src/main/java/com/example/semesterprojektbackend/model/Category.java
@@ -0,0 +1,9 @@
+package com.example.semesterprojektbackend.model;
+
+public class Category {
+    private String categoryName;
+    private String[] subCategory;
+    private int numberOfProducts;
+
+
+}
