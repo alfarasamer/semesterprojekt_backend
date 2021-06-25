a2da8f20bbead6d453f4823be61f8bf3d7f4f7bf
alfarasamer <85562845+alfarasamer@users.noreply.github.com>
2021-06-25 15:47:58 +0200

    Commit9

--- src/main/java/com/example/semesterprojektbackend/controller/ProductController.java
+++ src/main/java/com/example/semesterprojektbackend/controller/ProductController.java
@@ -1,9 +1,9 @@
 package com.example.semesterprojektbackend.controller;
 
-import com.example.semesterprojektbackend.model.Login;
 import com.example.semesterprojektbackend.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +21,20 @@ public class ProductController {
     private Product getDemoProduct() {
         return new Product(123,"T-shirt","L",1,2,true,99.5);
     }
+
+    @PostMapping("/products")
+    public Product postProduct() {
+        return postDemoProduct();
+    }
+
+    @PostMapping("/registration/{itemNumber}")
+    public Product postProductPath(@PathVariable int itemNumber) {
+        return postDemoProduct();
+    }
+
+    private Product postDemoProduct() {
+        return new Product(123,"Shirt", "XL", 12, true, 55.33);
+    }
+
+
 }
